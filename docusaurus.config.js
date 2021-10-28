7a91a8313b0f0c22620ece052acc09427bf910b9
Leyang Yu <september437@gmail.com>
2021-10-27 23:28:40 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://lyu4321.github.io',
+  baseUrl: '/docusaurus-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'lyu4321', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-website', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
